20c91de0e9da1577c40c2e64b57f975ee451767b
PilouSki <adamski.pierre.alexandre@gmail.com>
2018-02-15 16:24:01 +0100

    init

---
+++ js/initWindow.js
@@ -0,0 +1,54 @@
+var gl;
+var canvas,width,height;
+
+function initWindow() {
+  width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+  height = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+  try {
+    gl = htmlCanvas.getContext('webgl2') || htmlCanvas.getContext('webgl');
+    if ( ! gl ) {
+      let msg = "Error creating WebGL Context : Browser does not support WebGL";
+      alert(msg);
+      throw Error(msg);
+    }
+    gl.viewportWidth = width;
+    gl.viewportHeight = height;
+  }
+  catch (e) {
+    let msg = "Error creating WebGL Context : " + e.toString();
+    alert(msg);
+    throw Error(msg);
+  }
+}
+
+function initGL() {
+  canvas = document.getElementById("c1");
+
+  initWindow();
+  // Register an event listener to call the resizeCanvas() function
+  // each time the window is resized.
+  window.addEventListener('resize', initWindow, false);
+
+  initBuffers();
+  initShaders();
+  initTextures();
+
+  setDebugParam();
+
+  gl.clearColor(0., 0., 0., 0.);
+  gl.clearDepth(1.);
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+  gl.enable(gl.BLEND);
+  gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.LEQUAL);
+
+  interact();
+  animate();
+}
---
+++ js/parameters.js
@@ -0,0 +1,21 @@
+var localParam = new LocalParam();
+
+function LocalParam(){
+  this.camera = {
+    near : 5,
+    far : 530,
+    fov : 40,
+    rotate : [-0.3, 0, 0],
+    translate : [0, 0, -280],
+    eye : [0, 0, -64]
+  };
+
+  this.LODBias = 8.0;
+  this.millis = 0.0;
+  this.elapsed = 1.0;
+  this.timeNow = 0.0;
+  this.currentTime = 0.0;
+  this.lastTime = 0.0;
+  this.fps = 60.0;
+  this.fpsAverage = 60.0;
+}

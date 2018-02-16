866ae9260e3d19a8c357d5ae0e03a0b4fa0a16ec
MB-P_PILOU <mb-p_pilou@MB-P-PAA.local>
2018-02-16 01:08:22 +0100

    all good to go

--- js/initWindow.js
+++ js/initWindow.js
@@ -1,6 +1,13 @@
 var gl;
 var canvas, width, height;
 
+function start() {
+  // Register an event listener to call the resizeCanvas() function
+  // each time the window is resized.
+  window.addEventListener('resize', initWindow, false);
+  initWindow();
+}
+
 function initWindow() {
   width = window.innerWidth ||
     document.documentElement.clientWidth ||
@@ -11,8 +18,8 @@ function initWindow() {
 
   try {
     canvas = document.getElementById("c1");
-    canvas.width = width;
-    canvas.height = height;
+    //canvas.width = width;
+    //canvas.height = height;
 
     gl = canvas.getContext('webgl2') || canvas.getContext('webgl');
     if (!gl) {
@@ -20,8 +27,8 @@ function initWindow() {
       alert(msg);
       throw Error(msg);
     }
-    gl.viewportWidth = width;
-    gl.viewportHeight = height;
+    //gl.viewportWidth = width;
+    //gl.viewportHeight = height;
   } catch (e) {
     let msg = "Error creating WebGL Context : " + e.toString();
     alert(msg);
@@ -38,21 +45,18 @@ function initWindow() {
 }
 
 function initGL() {
-  // Register an event listener to call the resizeCanvas() function
-  // each time the window is resized.
-  window.addEventListener('resize', initWindow, false);
-
-  initBuffers();
+  //initTextures();
   initShaders();
-  initTextures();
+  initBuffers();
 
-  gl.enable(gl.BLEND);
-  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+  gl.clearColor(1, 0, 0, 1);
   gl.enable(gl.DEPTH_TEST);
-  gl.depthFunc(gl.LEQUAL);
-  gl.clearDepth(1);
-  gl.clearColor(0, 0, 0, 1);
+  //gl.depthFunc(gl.LEQUAL);
+  //gl.clearDepth(1);
+  //gl.enable(gl.BLEND);
+  //gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
-  interact();
-  animate();
+  //interact();
+  //animate();
+  drawScene();
 }
